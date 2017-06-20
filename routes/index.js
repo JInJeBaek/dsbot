cca5cb0995e87dc24c4eddf9ca96f8b9d396a1f2
finish <bjj4016@gmai.com>
2017-06-20 18:25:12 +0900

    fini

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,16 @@ router.get('/', function(req, res, next) {
 router.get('/test', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
+
+router.get('/keyboard', (req, res) => {
+  const menu = {
+      type: 'buttons',
+      buttons: ["메뉴2", "메뉴2", "메뉴3"]
+  };
+
+  res.set({
+      'content-type': 'application/json'
+  }).send(JSON.stringify(menu));
+});
+
 module.exports = router;
